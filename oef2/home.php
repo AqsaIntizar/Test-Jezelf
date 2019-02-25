3702821f6668dd3cfad3874c8ea921ea66d4ebdc
AqsaIntizar <aqsatje@live.be>
2019-02-25 22:50:44 +0100

    linked to nav.inc.php

---
+++ oef2/home.php
@@ -0,0 +1,23 @@
+<?php
+  $page = $_SERVER['SCRIPT_NAME'];
+  
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>Document</title>
+</head>
+<?php include_once('nav.inc.php'); ?>
+<body>
+    <h1>Home</h1>
+    <p>Welcome to PHP!</p>
+    
+
+    
+</body>
+<?php include_once('footer.inc.php'); ?>
+</html>
