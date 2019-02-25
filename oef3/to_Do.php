16f15eedb7a975d4697a7e005f19ca11a600e1e3
AqsaIntizar <aqsatje@live.be>
2019-02-25 23:00:27 +0100

    oef3 file added

---
+++ oef3/to_Do.php
@@ -0,0 +1,23 @@
+<?php
+$toDo = [
+    [
+      "beschrijving" => "series",
+      "uren" => 7,
+      "categorie" => "le work"
+    ],
+    [
+        "beschrijving" => "trainen",
+        "uren" => 2,
+        "categorie" => "thuis"
+    ],
+    [
+        "beschrijving" => "wenen",
+        "uren" => 1,
+        "categorie" => "thuis"
+    ],
+    [
+        "beschrijving" => "huiswerk",
+        "uren" => 6,
+        "categorie" => "school"
+    ]
+  ];
